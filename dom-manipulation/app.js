9d2b5c84f6f4961bc2e452a40d91baa13aea63d2
Guillermo García <guillermieres97@gmail.com>
2022-11-19 20:28:06 +0100

    modify app.js file of dom exercise

--- dom-manipulation/app.js
+++ dom-manipulation/app.js
@@ -1,1 +1,20 @@
 "use strict";
+
+const redBox = document.getElementById("red");
+const redBtn = document.getElementById("red-btn");
+
+const greenBox = document.getElementById("green");
+const greenBtn = document.getElementById("green-btn");
+
+const yellowBox = document.getElementById("yellow");
+const yellowBtn = document.getElementById("yellow-btn");
+
+redBtn.addEventListener("click", function () {
+  if(redBox.style.backgroundColor = "white") {
+    redBox.style.backgroundColor = "red";
+  }
+
+  if(redBox.style.backgroundColor = "red") {
+    redBox.style.backgroundColor = "white";
+  }
+});
