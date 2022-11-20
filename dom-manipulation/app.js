b8d948e7d242e0920915f2b15dfe28d87a922a7d
Guillermo García <guillermieres97@gmail.com>
2022-11-20 16:21:38 +0100

    dom manipulation exercise finished

--- dom-manipulation/app.js
+++ dom-manipulation/app.js
@@ -1,5 +1,6 @@
 "use strict";
 
+// dom elements
 const redBox = document.getElementById("red");
 const redBtn = document.getElementById("red-btn");
 
@@ -9,12 +10,62 @@ const greenBtn = document.getElementById("green-btn");
 const yellowBox = document.getElementById("yellow");
 const yellowBtn = document.getElementById("yellow-btn");
 
+// variables
+let redExe = "white";
+let greenExe = "white";
+let yellowExe = "white";
+
+// click events
 redBtn.addEventListener("click", function () {
-  if(redBox.style.backgroundColor = "white") {
+  if (redExe === "white") {
     redBox.style.backgroundColor = "red";
+
+    redExe = "red";
+
+    return;
   }
 
-  if(redBox.style.backgroundColor = "red") {
+  if (redExe === "red") {
     redBox.style.backgroundColor = "white";
+
+    redExe = "white";
+
+    return;
+  }
+});
+
+greenBtn.addEventListener("click", function () {
+  if (greenExe === "white") {
+    greenBox.style.backgroundColor = "green";
+
+    greenExe = "green";
+
+    return;
+  }
+
+  if (greenExe === "green") {
+    greenBox.style.backgroundColor = "white";
+
+    greenExe = "white";
+
+    return;
+  }
+});
+
+yellowBtn.addEventListener("click", function () {
+  if (yellowExe === "white") {
+    yellowBox.style.backgroundColor = "yellow";
+
+    yellowExe = "yellow";
+
+    return;
+  }
+
+  if (yellowExe === "yellow") {
+    yellowBox.style.backgroundColor = "white";
+
+    yellowExe = "white";
+
+    return;
   }
 });
